96711e3b1673cd6a0f38d19e771b8389b4f19980
Adam Helinski <helinski.adam@outlook.com>
2018-03-03 10:17:54 +0100

    Add user namespace
    This namespace requires all useful namespaces and java classes for repl
    development.

---
+++ dev/user.clj
@@ -0,0 +1,32 @@
+(ns user
+
+  "For daydreaming in the repl."
+
+  (:require [clojure.spec.alpha              :as s]
+            [clojure.spec.gen.alpha          :as gen]
+            [clojure.spec.test.alpha         :as st]
+            [clojure.test.check.clojure-test :as tt]
+            [clojure.test.check.generators   :as tgen]
+            [clojure.test.check.properties   :as tprop]
+            [clojure.test                    :as t]
+            [criterium.core                  :as ct]
+            [dvlopt.spi                      :as spi])
+  (:import (com.sun.jna Memory
+                        Native
+                        NativeLong
+                        Pointer)
+           (dvlopt.spi Spi
+                       Spi$Requests
+                       SpiIOCTransfer)
+           java.io.RandomAccessFile))
+
+
+
+
+;;;;;;;;;;
+
+
+(comment
+  
+
+  )
